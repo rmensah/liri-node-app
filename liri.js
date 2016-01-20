8f6f3c1da4a524167d945a160960841764861793
RMENS <icgc@RMs-MacBook-Pro.fios-router.home>
2016-01-20 04:41:34 -0500

    added twitter api call to liri.js

--- liri.js
+++ liri.js
@@ -1,1 +1,39 @@
-console.log(hi);
+
+var twitter = require('twitter');
+var client = require('./keys.js');
+var tweetsApi = 'https://api.twitter.com/1.1/statuses/retweets_of_me.json';
+
+function myTweets(){
+	client.getReTweetsOfMe('tweetsApi', function(error, tweets, response){
+  if(error) throw error;
+  console.log(tweets);  // The favorites. 
+  console.log(response);  // Raw response object. 
+});
+}
+
+
+
+    
+
+/*
+var Twitter = require('twitter');
+
+    //Get this data from your twitter apps dashboard
+		twitterKeys = require('./keys.js');
+    }
+
+    var Twitter = new Twitter(twitterKeys);
+
+    //Example calls
+
+    twitter.getUserTimeline({ screen_name: 'richiemens', count: '10'}, error, success);
+
+    twitter.getMentionsTimeline({ count: '10'}, error, success);
+
+    twitter.getHomeTimeline({ count: '10'}, error, success);
+
+    twitter.getReTweetsOfMe({ count: '10'}, error, success);
+
+    twitter.getTweet({ id: '1111111111'}, error, success);
+
+*/
